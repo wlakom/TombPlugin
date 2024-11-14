4515e42e11fa419201755e186a48f35df291000c
Władysław Komorek <46459787+wlakom@users.noreply.github.com>
2024-11-14 12:22:14 +0100

    Update SearchPersonRelationService.java
    Update to new tags

--- src/kendzi/josm/plugin/tomb/service/SearchPersonRelationService.java
+++ src/kendzi/josm/plugin/tomb/service/SearchPersonRelationService.java
@@ -44,13 +44,16 @@ public class SearchPersonRelationService {
             XPathExpression expr = XmlUtil.createXpath("//osm/relation");
 
             XPathExpression nameX = XmlUtil.createXpath("tag[@k='name']/@v");
-            XPathExpression lived_inX = XmlUtil.createXpath("tag[@k='lived_in']/@v");
             XPathExpression bornX = XmlUtil.createXpath("tag[@k='born']/@v");
             XPathExpression diedX = XmlUtil.createXpath("tag[@k='died']/@v");
 
             XPathExpression wikipediaX = XmlUtil.createXpath("tag[@k='wikipedia']/@v");
+            XPathExpression wikidataX = XmlUtil.createXpath("tag[@k='wikidata']/@v");
             XPathExpression descriptionX = XmlUtil.createXpath("tag[@k='description']/@v");
-            XPathExpression familyNameX = XmlUtil.createXpath("tag[@k='family_name']/@v");
+            XPathExpression family_nameX = XmlUtil.createXpath("tag[@k='family_name']/@v");
+            XPathExpression lived_inX = XmlUtil.createXpath("tag[@k='lived_in']/@v");
+            XPathExpression birthplaceX = XmlUtil.createXpath("tag[@k='birthplace']/@v");
+            XPathExpression deathplaceX = XmlUtil.createXpath("tag[@k='deathplace']/@v");
 
             NodeList nodes = XmlUtil.findNodes(doc, expr);
 
@@ -64,13 +67,16 @@ public class SearchPersonRelationService {
 
                 p.setName(XmlUtil.findString(node, nameX));
 
-                p.setLivedIn(XmlUtil.findString(node, lived_inX));
                 p.setBorn(XmlUtil.findString(node, bornX));
                 p.setDied(XmlUtil.findString(node, diedX));
 
                 p.setWikipedia(XmlUtil.findString(node, wikipediaX));
                 p.setDescription(XmlUtil.findString(node, descriptionX));
-                p.setFamilyName(XmlUtil.findString(node, familyNameX));
+                p.setFamily_name(XmlUtil.findString(node, family_nameX));
+                p.setLived_in(XmlUtil.findString(node, lived_inX));
+
+                p.setBirthplace(XmlUtil.findString(node, birthplaceX));
+                p.setDeathplace(XmlUtil.findString(node, deathplaceX));
 
                 ret.add(p);
             }
